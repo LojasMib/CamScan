b7f67e7d45a861d570d09d12abab6f21e9a76916
Magnoamaury <144865205+AmauryMagno@users.noreply.github.com>
2024-12-19 16:10:14 -0300

    Update
    Update configurations

--- code/PrintScanner/obj/x86/Debug/net8.0-windows8.0/Pages/Settings.g.i.cs
+++ code/PrintScanner/obj/x86/Debug/net8.0-windows8.0/Pages/Settings.g.i.cs
@@ -111,7 +111,7 @@ namespace CamScan.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CamScan;component/pages/settings.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CamScan;V1.0.0.0;component/pages/settings.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Pages\Settings.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
