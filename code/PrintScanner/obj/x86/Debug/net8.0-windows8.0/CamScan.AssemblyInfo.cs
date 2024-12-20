7ed2e5d14dbae4f4c3804bc7229a34ff628938e7
Magnoamaury <144865205+AmauryMagno@users.noreply.github.com>
2024-12-20 19:03:28 -0300

    Correction Error
    Page Scanner - correct error in max additions to page in PDF Confissão de Divida

--- code/PrintScanner/obj/x86/Debug/net8.0-windows8.0/CamScan.AssemblyInfo.cs
+++ code/PrintScanner/obj/x86/Debug/net8.0-windows8.0/CamScan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CamScan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d60ed8ffb3ad402e446e9a0b2dae68b467d0848")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef4479123cd1a507939072b18c3ccbc27471f4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CamScan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CamScan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
